c919bf17d5638b16a4529dbbd03f29ae521c9b3b
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-08-29 15:20:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from bardapi import Bard
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def summary(s1):
-    token = 'xxxxx'
+    token = 'xxxxxxxxx'
     bard = Bard(token=token)
-    return bard.get_answer(s1 + "\ncan you summarise this text for me in short?")['content']
+    ans =  bard.get_answer(s1 + "\ncan you summarise this text for me in short?")['content']
+    return ans.replace("I hope this summary is helpful. Let me know if you have any other questions.", "").strip()
 
 Vid = ""
 link = input("Enter link: ")
